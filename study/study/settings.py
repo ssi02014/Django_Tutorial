4b193006d7153df604e36b703b6cd62369f09071
JeonMinJae <ssi02014@naver.com>
2021-10-22 00:35:15 +0900

    Add(minjae): ALLOWED_HOSTS

--- study/study/settings.py
+++ study/study/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&&s7(@0(qg)bzc6ro04#$g_83y9g5apm4_olae66u#z7#_839e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.219.104', 'localhost', '127.0.0.1']
 
 
 # Application definition
